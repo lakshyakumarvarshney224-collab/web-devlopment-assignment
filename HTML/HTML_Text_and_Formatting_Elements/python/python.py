b47b39b3cb530eb5e41110d1c9e8a9b0b17a988a
lakshyakumarvarshney224-collab <lakshyakumarvarshney224@gmail.com>
2026-09-03 14:42:45 +0530

    added chagnes

---
+++ HTML/HTML_Text_and_Formatting_Elements/python/python.py
@@ -0,0 +1,7 @@
+strl="python is programing language and it is easy to learn."
+11=["ruby" "python","java","c++"]
+12=[1, 2, 3, 4, 5]
+output = print("YES") if "python" in strl else print("NO")
+output1 = print("YES") if "python" in 11 else print("NO")
+output2 = print("YES") if 3 is not in 12 else print("NO")
+print(output)
